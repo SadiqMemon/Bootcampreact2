7695720ff08d0b951fe45549b4cce893709c2709
Muhammad Sadiq <66436836+SadiqMemon@users.noreply.github.com>
2020-06-08 13:38:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <Dinner dishName="Chicken Briyani" Days="Sunday" sweetDish="Kheer" />
       <Dinner dishName="Chicken Karahi" Days="Monday" sweetDish="Lab e Shireen" />
       <Dinner dishName="Chicken Karahi" Days="Tuesday" sweetDish="Gajar ka Halwa" />
+      <Dinner dishName="Chicken pulao" Days="Wednesday" sweetDish="Jalebi" />
       </div>
   )
     
